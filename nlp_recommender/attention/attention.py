1fd780bc08e2536ae5b1f76d9f7d9eecc8fab39e
huichuan <lhc14124908@163.com>
2021-09-19 18:36:26 +0800

    ad d

--- nlp_recommender/attention/attention.py
+++ nlp_recommender/attention/attention.py
@@ -6,65 +6,25 @@
 
 from tensorflow.keras.layers import *
 
+'''
+inputs是一个二阶以上的张量，代表输入序列，比如形如(batch_size, seq_len, input_size)的张量；
+seq_len是一个形如(batch_size,)的张量，代表每个序列的实际长度，多出部分都被忽略；
+mode分为mul和add，mul是指把多出部分全部置零，一般用于全连接层之前；
+add是指把多出部分全部减去一个大的常数，一般用于softmax之前。
+'''
 
-def to_mask(x, mask, mode='mul'):
-    """通用mask函数
-    这里的mask.shape=[batch_size, seq_len]或[batch_size, seq_len, 1]
-    """
-    if mask is None:
-        return x
+
+def Mask(inputs, seq_len, mode='mul'):
+    if seq_len == None:
+        return inputs
     else:
-        for _ in range(K.ndim(x) - K.ndim(mask)):
-            mask = K.expand_dims(mask, K.ndim(mask))
+        mask = tf.cast(tf.sequence_mask(seq_len), tf.float32)
+        for _ in range(len(inputs.shape) - 2):
+            mask = tf.expand_dims(mask, 2)
         if mode == 'mul':
-            return x * mask
-        else:
-            return x - (1 - mask) * 1e10
-
-
-class OurLayer(Layer):
-    """定义新的Layer，增加reuse方法，允许在定义Layer时调用现成的层
-    """
-
-    def reuse(self, layer, *args, **kwargs):
-        if not layer.built:
-            if len(args) > 0:
-                inputs = args[0]
-            else:
-                inputs = kwargs['inputs']
-            if isinstance(inputs, list):
-                input_shape = [tf.shape(x) for x in inputs]
-            else:
-                input_shape = tf.shape(inputs)
-            layer.build(input_shape)
-        outputs = layer.call(*args, **kwargs)
-        for w in layer.trainable_weights:
-            if w not in self._trainable_weights:
-                self._trainable_weights.append(w)
-        for w in layer.non_trainable_weights:
-            if w not in self._non_trainable_weights:
-                self._non_trainable_weights.append(w)
-        for u in layer.updates:
-            if not hasattr(self, '_updates'):
-                self._updates = []
-            if u not in self._updates:
-                self._updates.append(u)
-        return outputs
-
-
-def extract_seq_patches(x, kernel_size, rate):
-    """x.shape = [None, seq_len, seq_dim]
-    滑动地把每个窗口的x取出来，为做局部attention作准备。
-    """
-    seq_dim = K.int_shape(x)[-1]
-    seq_len = K.shape(x)[1]
-    k_size = kernel_size + (rate - 1) * (kernel_size - 1)
-    p_right = (k_size - 1) // 2
-    p_left = k_size - 1 - p_right
-    x = K.temporal_padding(x, (p_left, p_right))
-    xs = [x[:, i: i + seq_len] for i in range(0, k_size, rate)]
-    x = K.concatenate(xs, 2)
-    return K.reshape(x, (-1, seq_len, kernel_size, seq_dim))
+            return inputs * mask
+        if mode == 'add':
+            return inputs - (1 - mask) * 1e12
 
 
 class Attention(OurLayer):
@@ -88,48 +48,38 @@ class Attention(OurLayer):
 
     def call(self, inputs):
         q, k, v = inputs[: 3]
-        v_mask, q_mask = None, None
+
+        V_len, Q_len = None, None
         # 这里的mask.shape=[batch_size, seq_len]或[batch_size, seq_len, 1]
         if len(inputs) > 3:
-            v_mask = inputs[3]
+            V_len = inputs[3]
             if len(inputs) > 4:
-                q_mask = inputs[4]
+                Q_len = inputs[4]
         # 线性变换
-        qw = self.reuse(self.q_dense, q)
-        kw = self.reuse(self.k_dense, k)
-        vw = self.reuse(self.v_dense, v)
-        # 形状变换
-        qw = tf.reshape(qw, (-1, K.shape(qw)[1], self.heads, self.key_size))
-        kw = tf.reshape(kw, (-1, K.shape(kw)[1], self.heads, self.key_size))
-        vw = tf.reshape(vw, (-1, K.shape(vw)[1], self.heads, self.size_per_head))
-        # 维度置换
-        qw = tf.transpose(qw, (0, 2, 1, 3))
-        kw = tf.transpose(kw, (0, 2, 1, 3))
-        vw = tf.transpose(vw, (0, 2, 1, 3))
+        qw = self.q_dense(q)
+        qw = tf.reshape(qw, (-1, tf.shape(qw)[1], self.heads, self.key_size))
+        qw = tf.transpose(qw, [0, 2, 1, 3])
+
+        kw = self.k_dense(k)
+        kw = tf.reshape(kw, (-1, tf.shape(kw)[1], self.heads, self.key_size))
+        kw = tf.transpose(kw, [0, 2, 1, 3])
+
+        vw = self.v_dense(v)
+        vw = tf.reshape(vw, (-1, tf.shape(vw)[1], self.heads, self.key_size))
+        vw = tf.transpose(vw, [0, 2, 1, 3])
         # Attention
-        a = K.batch_dot(qw, kw, [3, 3]) / self.key_size ** 0.5
-        a = K.permute_dimensions(a, (0, 3, 2, 1))
-        a = to_mask(a, v_mask, 'add')
-        a = K.permute_dimensions(a, (0, 3, 2, 1))
-        if (self.mask_right is not False) or (self.mask_right is not None):
-            if self.mask_right is True:
-                ones = K.ones_like(a[: 1, : 1])
-                mask = (ones - K.tf.matrix_band_part(ones, -1, 0)) * 1e10
-                a = a - mask
-            else:
-                # 这种情况下，mask_right是外部传入的0/1矩阵，shape=[q_len, k_len]
-                mask = (1 - K.constant(self.mask_right)) * 1e10
-                mask = K.expand_dims(K.expand_dims(mask, 0), 0)
-                self.mask = mask
-                a = a - mask
-        a = K.softmax(a)
-        self.a = a
-        # 完成输出
-        o = K.batch_dot(a, vw, [3, 2])
-        o = K.permute_dimensions(o, (0, 2, 1, 3))
-        o = K.reshape(o, (-1, K.shape(o)[1], self.out_dim))
-        o = to_mask(o, q_mask, 'mul')
-        return o
+
+        A = tf.matmul(qw, kw, transpose_b=True) / tf.sqrt(float(self.key_size))
+        A = tf.transpose(A, [0, 3, 2, 1])
+        A = Mask(A, V_len, mode='add')
+        A = tf.transpose(A, [0, 3, 2, 1])
+        A = tf.nn.softmax(A)
+        # 输出并mask
+        O = tf.matmul(A, vw)
+        O = tf.transpose(O, [0, 2, 1, 3])
+        O = tf.reshape(O, (-1, tf.shape(O)[1], self.heads * self.key_size))
+        O = Mask(O, Q_len, 'mul')
+        return O
 
     def compute_output_shape(self, input_shape):
         return (input_shape[0][0], input_shape[0][1], self.out_dim)
@@ -159,11 +109,11 @@ S_inputs = tf.keras.Input(shape=(None,), dtype='int32')
 embeddings = tf.keras.layers.Embedding(max_features, 128)(S_inputs)
 # embeddings = SinCosPositionEmbedding(128)(embeddings) # 增加Position_Embedding能轻微提高准确率
 O_seq = Attention(8, 16)([embeddings, embeddings, embeddings])
-O_seq = GlobalAveragePooling1D()(O_seq)
-O_seq = Dropout(0.5)(O_seq)
-outputs = Dense(1, activation='sigmoid')(O_seq)
+O_seq = tf.keras.layers.GlobalAveragePooling1D()(O_seq)
+O_seq = tf.keras.layers.Dropout(0.5)(O_seq)
+outputs = tf.keras.layers.Dense(1, activation='sigmoid')(O_seq)
 
-model = Model(inputs=S_inputs, outputs=outputs)
+model = tf.keras.Model(inputs=S_inputs, outputs=outputs)
 # try using different optimizers and different optimizer configs
 model.compile(loss='binary_crossentropy',
               optimizer='adam',
