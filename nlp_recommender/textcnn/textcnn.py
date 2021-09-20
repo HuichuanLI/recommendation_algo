e32ad1666c4cb98eda3808ff134d6482a89d8368
huichuan <lhc14124908@163.com>
2021-09-20 14:09:39 +0800

    add

---
+++ nlp_recommender/textcnn/textcnn.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+# @Time : 2021/9/20 10:07 上午
+# @Author : huichuan LI
+# @File : textcnn.py
+# @Software: PyCharm
---
+++ nlp_recommender/textcnn/textlstm.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+# @Time : 2021/9/20 1:51 下午
+# @Author : huichuan LI
+# @File : textlstm.py
+# @Software: PyCharm
