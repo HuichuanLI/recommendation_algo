43c74215af4825152f25452f9532ae0f218065c7
huichuan <lhc14124908@163.com>
2021-09-21 17:28:47 +0800

    add

---
+++ nlp_recommender/Transformer/gpt.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+# @Time : 2021/9/21 5:28 下午
+# @Author : huichuan LI
+# @File : gpt.py
+# @Software: PyCharm
--- nlp_recommender/Transformer/transformer.py
+++ nlp_recommender/Transformer/transformer.py
@@ -8,6 +8,7 @@ import tensorflow as tf
 from tensorflow import keras
 
 import utils
+import time
 
 MODEL_DIM = 32
 MAX_LEN = 12
@@ -17,25 +18,27 @@ DATA_SIZE = 6400
 BATCH_SIZE = 64
 LEARN_RATE = 0.001
 EPOCHS = 60
+DROP_RATE = 0.1
 
 
 class MultiHead(keras.layers.Layer):
-    def __init__(self, n_head):
+
+    def __init__(self, n_head, model_dim, drop_rate):
         super().__init__()
+        self.head_dim = model_dim // n_head
         self.n_head = n_head
+        self.model_dim = model_dim
+        self.wq = keras.layers.Dense(n_head * self.head_dim)
+        self.wk = keras.layers.Dense(n_head * self.head_dim)
+        self.wv = keras.layers.Dense(n_head * self.head_dim)  # [n, step, h*h_dim]
+
+        self.o_dense = keras.layers.Dense(model_dim)
+        self.o_drop = keras.layers.Dropout(rate=drop_rate)
         self.attention = None
 
-    def build(self, input_shape):
-        (q_b, q_t, q_f), (k_b, k_t, k_f), (v_b, v_t, v_f) = input_shape
-        self.k_f = tf.cast(q_f, tf.float32)
-        h_dim = q_f // self.n_head
-        self.wq = self.add_weight('wq', [self.n_head, q_f, h_dim])
-        self.wk = self.add_weight('wk', [self.n_head, k_f, h_dim])
-        self.wv = self.add_weight('wv', [self.n_head, v_f, h_dim])
-        self.wo = self.add_weight('wo', [self.n_head * h_dim, v_f])
-        super(MultiHead, self).build(input_shape)
-
     def call(self, q, k, v, mask, training):
+        print(self.n_head)
+        print(self.head_dim)
         _q = self.wq(q)  # [n, q_step, h*h_dim]
         _k, _v = self.wk(k), self.wv(v)  # [n, step, h*h_dim]
         _q = self.split_heads(_q)  # [n, h, q_step, h_dim]
@@ -46,18 +49,19 @@ class MultiHead(keras.layers.Layer):
         return o
 
     def split_heads(self, x):
-        x = tf.reshape(x, (x.shape[0], x.shape[1], self.n_head, self.head_dim))  # [n, step, h, h_dim]
+        x = tf.reshape(x, (-1, x.shape[1], self.n_head, self.head_dim))  # [n, step, h, h_dim]
         return tf.transpose(x, perm=[0, 2, 1, 3])  # [n, h, step, h_dim]
 
     def scaled_dot_product_attention(self, q, k, v, mask=None):
         dk = tf.cast(k.shape[-1], dtype=tf.float32)
         score = tf.matmul(q, k, transpose_b=True) / (tf.math.sqrt(dk) + 1e-8)  # [n, h_dim, q_step, step]
+
         if mask is not None:
             score += mask * -1e9
         self.attention = tf.nn.softmax(score, axis=-1)  # [n, h, q_step, step]
         context = tf.matmul(self.attention, v)  # [n, h, q_step, step] @ [n, h, step, dv] = [n, h, q_step, dv]
         context = tf.transpose(context, perm=[0, 2, 1, 3])  # [n, q_step, h, dv]
-        context = tf.reshape(context, (context.shape[0], context.shape[1], -1))  # [n, q_step, h*dv]
+        context = tf.reshape(context, (-1, context.shape[1], context.shape[2] * context.shape[3]))  # [n, q_step, h*dv]
         return context
 
 
@@ -183,3 +187,110 @@ class Transformer(keras.Model):
         mask = 1 - tf.linalg.band_part(tf.ones((self.max_len, self.max_len)), -1, 0)
         mask = tf.where(self._pad_bool(seqs)[:, tf.newaxis, tf.newaxis, :], 1, mask[tf.newaxis, tf.newaxis, :, :])
         return mask  # (step, step)
+
+    def step(self, x, y):
+        with tf.GradientTape() as tape:
+            logits = self.call(x, y, training=True)
+            pad_mask = tf.math.not_equal(y, self.padding_idx)
+            loss = tf.reduce_mean(tf.boolean_mask(self.cross_entropy(y, logits), pad_mask))
+        grads = tape.gradient(loss, self.trainable_variables)
+        self.opt.apply_gradients(zip(grads, self.trainable_variables))
+        return loss, logits
+
+    def translate(self, src, v2i, i2v):
+        src_pad = utils.pad_zero(src, self.max_len)
+        tgt = utils.pad_zero(np.array([[v2i["<GO>"], ] for _ in range(len(src))]), self.max_len + 1)
+        tgti = 0
+        x_embed = self.embed(src_pad)
+        encoded_z = self.encoder.call(x_embed, False, mask=self._pad_mask(src_pad))
+        while True:
+            y = tgt[:, :-1]
+            y_embed = self.embed(y)
+            decoded_z = self.decoder.call(
+                y_embed, encoded_z, False, yz_look_ahead_mask=self._look_ahead_mask(y),
+                xz_pad_mask=self._pad_mask(src_pad))
+            logits = self.o(decoded_z)[:, tgti, :].numpy()
+            idx = np.argmax(logits, axis=1)
+            tgti += 1
+            tgt[:, tgti] = idx
+            if tgti >= self.max_len:
+                break
+        return ["".join([i2v[i] for i in tgt[j, 1:tgti]]) for j in range(len(src))]
+
+    @property
+    def attentions(self):
+        attentions = {
+            "encoder": [l.mh.attention.numpy() for l in self.encoder.ls],
+            "decoder": {
+                "mh1": [l.mh[0].attention.numpy() for l in self.decoder.ls],
+                "mh2": [l.mh[1].attention.numpy() for l in self.decoder.ls],
+            }}
+        return attentions
+
+
+if __name__ == "__main__":
+    max_features = 20000
+    maxlen = 80
+    batch_size = 32
+
+    m = Transformer(MODEL_DIM, maxlen, N_LAYER, N_HEAD, n_vocab=max_features, drop_rate=DROP_RATE)
+    from keras.preprocessing import sequence
+    from keras.datasets import imdb
+    import tensorflow as tf
+
+    print('Loading data...')
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.imdb.load_data(
+        num_words=max_features, maxlen=maxlen
+    )
+    print(len(x_train), 'train sequences')
+    print(len(x_test), 'test sequences')
+    print('Pad sequences (samples x time)')
+    x_train = sequence.pad_sequences(x_train, maxlen=maxlen, padding='post')
+    x_test = sequence.pad_sequences(x_test, maxlen=maxlen, padding='post')
+    print('x_train shape:', x_train.shape)
+    print('x_test shape:', x_test.shape)
+    step = 10
+
+    S_inputs = tf.keras.Input(shape=(None,), dtype='int32')
+    model_dim = MODEL_DIM
+    max_len = maxlen
+
+    n_vocab = max_features
+    n_head = N_HEAD
+    drop_rate = DROP_RATE
+    n_layer = 3
+    training = True
+
+    embed = PositionEmbedding(max_len, model_dim, n_vocab)
+    encoder = Encoder(n_head, model_dim, drop_rate, n_layer)
+    x_embed = embed(S_inputs)
+    pad_mask = m._pad_mask(S_inputs)
+    encoded_z = encoder.call(x_embed, training, mask=pad_mask)
+    # embeddings = SinCosPositionEmbedding(128)(embeddings) # 增加Position_Embedding能轻微提高准确率
+    O_seq = tf.keras.layers.GlobalAveragePooling1D()(encoded_z)
+    O_seq = tf.keras.layers.Dropout(0.5)(O_seq)
+    outputs = tf.keras.layers.Dense(1, activation='sigmoid')(O_seq)
+
+    model = tf.keras.Model(inputs=S_inputs, outputs=outputs)
+    print(model.summary())
+    # try using different optimizers and different optimizer configs
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+
+    print('Train...')
+    model.fit(x_train, y_train,
+              batch_size=batch_size,
+              epochs=100,
+              validation_data=(x_test, y_test))
+
+    # t0 = time.time()
+    # for t in range(step):
+    #     batches = len(x_train) // batch_size
+    #     for b in range(batches):
+    #         loss, logits = m.step(x_train[b * batch_size:b * batch_size + BATCH_SIZE, :],
+    #                               x_train[b * batch_size:b * batch_size + BATCH_SIZE, :])
+    #     if t % 50 == 0:
+    #         logits = logits[0].numpy()
+    #         t1 = time.time()
+    #         t0 = t1
