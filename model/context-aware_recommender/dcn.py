b783671a16a307526a054d84e3f3a0e423ccc3c9
huichuan <lhc14124908@163.com>
2022-03-04 00:23:08 +0800

    add

--- model/context-aware_recommender/dcn.py
+++ model/context-aware_recommender/dcn.py
@@ -2,24 +2,111 @@
 # @Time : 2022/2/28 11:19 下午
 # @Author : huichuan LI
 # @File : dcn.py
-# @Software: PyCharm
-# -*- coding: utf-8 -*-
-# @Time    : 2021/10/8 15:33
-# @Author  : Li Huichuan
-# @File    : FM.py
-# @Software: PyCharm
 import tensorflow as tf
 from tensorflow.keras.layers import *
 from tensorflow.keras.models import *
 from tensorflow.keras.layers import Layer
-
+import tensorflow as tf
+from tensorflow.python.keras import backend as K
+from tensorflow.python.keras.backend import batch_dot
+from tensorflow.python.keras.initializers import (Zeros, glorot_normal,
+                                                  glorot_uniform, TruncatedNormal)
 import pandas as pd
 import numpy as np
 from collections import namedtuple
+from utils import DNN
+from tensorflow.python.keras.regularizers import l2
 
 SparseFeat = namedtuple('SparseFeat', ['name', 'vocabulary_size', 'embedding_dim'])
 DenseFeat = namedtuple('DenseFeat', ['name', 'dimension'])
 VarLenSparseFeat = namedtuple('VarLenSparseFeat', ['name', 'vocabulary_size', 'embedding_dim', 'maxlen'])
+
+
+class CrossNet(Layer):
+    """The Cross Network part of Deep&Cross Network model,
+    which leans both low and high degree cross feature.
+      Input shape
+        - 2D tensor with shape: ``(batch_size, units)``.
+      Output shape
+        - 2D tensor with shape: ``(batch_size, units)``.
+      Arguments
+        - **layer_num**: Positive integer, the cross layer number
+        - **l2_reg**: float between 0 and 1. L2 regularizer strength applied to the kernel weights matrix
+        - **parameterization**: string, ``"vector"``  or ``"matrix"`` ,  way to parameterize the cross network.
+        - **seed**: A Python integer to use as random seed.
+      References
+        - [Wang R, Fu B, Fu G, et al. Deep & cross network for ad click predictions[C]//Proceedings of the ADKDD'17. ACM, 2017: 12.](https://arxiv.org/abs/1708.05123)
+    """
+
+    def __init__(self, layer_num=2, parameterization='vector', l2_reg=0, seed=1024, **kwargs):
+        self.layer_num = layer_num
+        self.parameterization = parameterization
+        self.l2_reg = l2_reg
+        self.seed = seed
+        print('CrossNet parameterization:', self.parameterization)
+        super(CrossNet, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+
+        if len(input_shape) != 2:
+            raise ValueError(
+                "Unexpected inputs dimensions %d, expect to be 2 dimensions" % (len(input_shape),))
+
+        dim = int(input_shape[-1])
+        if self.parameterization == 'vector':
+            self.kernels = [self.add_weight(name='kernel' + str(i),
+                                            shape=(dim, 1),
+                                            initializer=glorot_normal(
+                                                seed=self.seed),
+                                            regularizer=l2(self.l2_reg),
+                                            trainable=True) for i in range(self.layer_num)]
+        elif self.parameterization == 'matrix':
+            self.kernels = [self.add_weight(name='kernel' + str(i),
+                                            shape=(dim, dim),
+                                            initializer=glorot_normal(
+                                                seed=self.seed),
+                                            regularizer=l2(self.l2_reg),
+                                            trainable=True) for i in range(self.layer_num)]
+        else:  # error
+            raise ValueError("parameterization should be 'vector' or 'matrix'")
+        self.bias = [self.add_weight(name='bias' + str(i),
+                                     shape=(dim, 1),
+                                     initializer=Zeros(),
+                                     trainable=True) for i in range(self.layer_num)]
+        # Be sure to call this somewhere!
+        super(CrossNet, self).build(input_shape)
+
+    def call(self, inputs, **kwargs):
+        if K.ndim(inputs) != 2:
+            raise ValueError(
+                "Unexpected inputs dimensions %d, expect to be 2 dimensions" % (K.ndim(inputs)))
+
+        x_0 = tf.expand_dims(inputs, axis=2)
+        x_l = x_0
+        for i in range(self.layer_num):
+            if self.parameterization == 'vector':
+                xl_w = tf.tensordot(x_l, self.kernels[i], axes=(1, 0))
+                dot_ = tf.matmul(x_0, xl_w)
+                x_l = dot_ + self.bias[i] + x_l
+            elif self.parameterization == 'matrix':
+                xl_w = tf.einsum('ij,bjk->bik', self.kernels[i], x_l)  # W * xi  (bs, dim, 1)
+                dot_ = xl_w + self.bias[i]  # W * xi + b
+                x_l = x_0 * dot_ + x_l  # x0 · (W * xi + b) +xl  Hadamard-product
+            else:  # error
+                raise ValueError("parameterization should be 'vector' or 'matrix'")
+        x_l = tf.squeeze(x_l, axis=2)
+        return x_l
+
+    def get_config(self, ):
+
+        config = {'layer_num': self.layer_num, 'parameterization': self.parameterization,
+                  'l2_reg': self.l2_reg, 'seed': self.seed}
+        base_config = super(CrossNet, self).get_config()
+        base_config.update(config)
+        return base_config
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
 
 
 def build_input_layers(feature_columns):
@@ -67,7 +154,10 @@ def build_embedding_layers(feature_columns, input_layer_dict):
     return embedding_layer_dict
 
 
-def DCN(feature_columns):
+def DCN(feature_columns, cross_num=2, cross_parameterization='vector',
+        dnn_hidden_units=(256, 128, 64), l2_reg_linear=1e-5, l2_reg_embedding=1e-5,
+        l2_reg_cross=1e-5, l2_reg_dnn=0, seed=1024, dnn_dropout=0, dnn_use_bn=False,
+        dnn_activation='relu', task='binary'):
     input_layer_dict = build_input_layers(feature_columns)
 
     input_layers = list(input_layer_dict.values())
@@ -91,7 +181,25 @@ def DCN(feature_columns):
     emb_input = Concatenate(axis=1)(dnn_sparse_embed_input)
 
     input = Concatenate(axis=1)([emb_input, dnn_dense_input])
-    output = tf.math.sigmoid(tf.keras.layers.Dense(1)(input))
+
+    if len(dnn_hidden_units) > 0 and cross_num > 0:  # Deep & Cross
+        deep_out = DNN(dnn_hidden_units, dnn_activation, l2_reg_dnn, dnn_dropout, dnn_use_bn, seed=seed)(input)
+        cross_out = CrossNet(cross_num, parameterization=cross_parameterization, l2_reg=l2_reg_cross)(input)
+        stack_out = tf.keras.layers.Concatenate()([cross_out, deep_out])
+        final_logit = tf.keras.layers.Dense(
+            1, use_bias=False, kernel_initializer=tf.keras.initializers.glorot_normal(seed))(stack_out)
+    elif len(dnn_hidden_units) > 0:  # Only Deep
+        deep_out = DNN(dnn_hidden_units, dnn_activation, l2_reg_dnn, dnn_dropout, dnn_use_bn, seed=seed)(input)
+        final_logit = tf.keras.layers.Dense(
+            1, use_bias=False, kernel_initializer=tf.keras.initializers.glorot_normal(seed))(deep_out)
+    elif cross_num > 0:  # Only Cross
+        cross_out = CrossNet(cross_num, parameterization=cross_parameterization, l2_reg=l2_reg_cross)(input)
+        final_logit = tf.keras.layers.Dense(
+            1, use_bias=False, kernel_initializer=tf.keras.initializers.glorot_normal(seed))(cross_out)
+    else:  # Error
+        raise NotImplementedError
+
+    output = tf.math.sigmoid(tf.keras.layers.Dense(1)(input) + final_logit)
     model = Model(input_layers, output)
     return model
 
@@ -129,12 +237,12 @@ if __name__ == "__main__":
     n_users = max(samples_data["user_id"]) + 1
     n_item = max(samples_data["movie_id"]) + 1
 
-    lr = LR(feature_columns)
+    dcn = DCN(feature_columns)
     #
-    lr.compile('adam',
+    dcn.compile('adam',
                loss=tf.keras.losses.BinaryCrossentropy(),
                metrics=[tf.keras.metrics.BinaryAccuracy(),
                         tf.keras.metrics.AUC()])
-    lr.fit(X_train, y_train, batch_size=64, epochs=10, validation_split=0.2, )
+    dcn.fit(X_train, y_train, batch_size=64, epochs=10, validation_split=0.2, )
     #
-    print(lr.summary())
+    print(dcn.summary())
