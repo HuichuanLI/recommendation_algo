41331d050ca328f1eda96dc535f306f9f58214ac
huichuan <lhc14124908@163.com>
2022-03-05 10:55:02 +0800

    add

--- model/context-aware_recommender/autoint.py
+++ model/context-aware_recommender/autoint.py
@@ -3,3 +3,275 @@
 # @Author : huichuan LI
 # @File : autoint.py
 # @Software: PyCharm
+
+from tensorflow.keras.layers import *
+from tensorflow.keras.models import *
+from tensorflow.keras.layers import Layer
+import tensorflow as tf
+from tensorflow.python.keras import backend as K
+from tensorflow.python.keras.backend import batch_dot
+from tensorflow.python.keras.initializers import (Zeros, glorot_normal,
+                                                  glorot_uniform, TruncatedNormal)
+import pandas as pd
+import numpy as np
+from collections import namedtuple
+from utils import DNN
+from tensorflow.python.keras.regularizers import l2
+
+SparseFeat = namedtuple('SparseFeat', ['name', 'vocabulary_size', 'embedding_dim'])
+DenseFeat = namedtuple('DenseFeat', ['name', 'dimension'])
+VarLenSparseFeat = namedtuple('VarLenSparseFeat', ['name', 'vocabulary_size', 'embedding_dim', 'maxlen'])
+
+
+class InteractingLayer(Layer):
+    """A Layer used in AutoInt that model the correlations between different feature fields by multi-head self-attention mechanism.
+      Input shape
+            - A 3D tensor with shape: ``(batch_size,field_size,embedding_size)``.
+      Output shape
+            - 3D tensor with shape:``(batch_size,field_size,att_embedding_size * head_num)``.
+      Arguments
+            - **att_embedding_size**: int.The embedding size in multi-head self-attention network.
+            - **head_num**: int.The head number in multi-head  self-attention network.
+            - **use_res**: bool.Whether or not use standard residual connections before output.
+            - **seed**: A Python integer to use as random seed.
+      References
+            - [Song W, Shi C, Xiao Z, et al. AutoInt: Automatic Feature Interaction Learning via Self-Attentive Neural Networks[J]. arXiv preprint arXiv:1810.11921, 2018.](https://arxiv.org/abs/1810.11921)
+    """
+
+    def __init__(self, att_embedding_size=8, head_num=2, use_res=True, scaling=False, seed=1024, **kwargs):
+        if head_num <= 0:
+            raise ValueError('head_num must be a int > 0')
+        self.att_embedding_size = att_embedding_size
+        self.head_num = head_num
+        self.use_res = use_res
+        self.seed = seed
+        self.scaling = scaling
+        super(InteractingLayer, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        if len(input_shape) != 3:
+            raise ValueError(
+                "Unexpected inputs dimensions %d, expect to be 3 dimensions" % (len(input_shape)))
+        embedding_size = int(input_shape[-1])
+        self.W_Query = self.add_weight(name='query', shape=[embedding_size, self.att_embedding_size * self.head_num],
+                                       dtype=tf.float32,
+                                       initializer=tf.keras.initializers.TruncatedNormal(seed=self.seed))
+        self.W_key = self.add_weight(name='key', shape=[embedding_size, self.att_embedding_size * self.head_num],
+                                     dtype=tf.float32,
+                                     initializer=tf.keras.initializers.TruncatedNormal(seed=self.seed + 1))
+        self.W_Value = self.add_weight(name='value', shape=[embedding_size, self.att_embedding_size * self.head_num],
+                                       dtype=tf.float32,
+                                       initializer=tf.keras.initializers.TruncatedNormal(seed=self.seed + 2))
+        if self.use_res:
+            self.W_Res = self.add_weight(name='res', shape=[embedding_size, self.att_embedding_size * self.head_num],
+                                         dtype=tf.float32,
+                                         initializer=tf.keras.initializers.TruncatedNormal(seed=self.seed))
+
+        # Be sure to call this somewhere!
+        super(InteractingLayer, self).build(input_shape)
+
+    def call(self, inputs, **kwargs):
+        if K.ndim(inputs) != 3:
+            raise ValueError(
+                "Unexpected inputs dimensions %d, expect to be 3 dimensions" % (K.ndim(inputs)))
+        querys = tf.tensordot(inputs, self.W_Query,
+                              axes=(-1, 0))  # None F D*head_num
+        keys = tf.tensordot(inputs, self.W_key, axes=(-1, 0))
+        values = tf.tensordot(inputs, self.W_Value, axes=(-1, 0))
+
+        # head_num None F D
+        querys = tf.stack(tf.split(querys, self.head_num, axis=2))
+        keys = tf.stack(tf.split(keys, self.head_num, axis=2))
+        values = tf.stack(tf.split(values, self.head_num, axis=2))
+
+        inner_product = tf.matmul(
+            querys, keys, transpose_b=True)  # head_num None F F
+        if self.scaling:
+            inner_product /= self.att_embedding_size ** 0.5
+        self.normalized_att_scores = tf.nn.softmax(inner_product)
+
+        result = tf.matmul(self.normalized_att_scores,
+                           values)  # head_num None F D
+        result = tf.concat(tf.split(result, self.head_num, ), axis=-1)
+        result = tf.squeeze(result, axis=0)  # None F D*head_num
+
+        if self.use_res:
+            result += tf.tensordot(inputs, self.W_Res, axes=(-1, 0))
+        result = tf.nn.relu(result)
+
+        return result
+
+    def compute_output_shape(self, input_shape):
+
+        return (None, input_shape[1], self.att_embedding_size * self.head_num)
+
+    def get_config(self, ):
+        config = {'att_embedding_size': self.att_embedding_size, 'head_num': self.head_num, 'use_res': self.use_res,
+                  'seed': self.seed}
+        base_config = super(InteractingLayer, self).get_config()
+        base_config.update(config)
+        return base_config
+
+
+def build_input_layers(feature_columns):
+    input_layer_dict = {}
+
+    for fc in feature_columns:
+        if isinstance(fc, SparseFeat):
+            input_layer_dict[fc.name] = Input(shape=(1,), name=fc.name)
+        elif isinstance(fc, DenseFeat):
+            input_layer_dict[fc.name] = Input(shape=(fc.dimension,), name=fc.name)
+        elif isinstance(fc, VarLenSparseFeat):
+            input_layer_dict[fc.name] = Input(shape=(fc.maxlen,), name=fc.name)
+
+    return input_layer_dict
+
+
+# 将所有的sparse特征embedding拼接
+def concat_embedding_list(feature_columns, input_layer_dict, embedding_layer_dict, flatten=False):
+    embedding_list = []
+    for fc in feature_columns:
+        _input = input_layer_dict[fc.name]  # 获取输入层
+        _embed = embedding_layer_dict[fc.name]  # B x 1 x dim  获取对应的embedding层
+        embed = _embed(_input)  # B x dim  将input层输入到embedding层中
+
+        # 是否需要flatten, 如果embedding列表最终是直接输入到Dense层中，需要进行Flatten，否则不需要
+        if flatten:
+            embed = Flatten()(embed)
+
+        embedding_list.append(embed)
+
+    return embedding_list
+
+
+# 构建embedding层
+def build_embedding_layers(feature_columns, input_layer_dict):
+    embedding_layer_dict = {}
+
+    for fc in feature_columns:
+        if isinstance(fc, SparseFeat):
+            embedding_layer_dict[fc.name] = Embedding(fc.vocabulary_size, fc.embedding_dim, name='emb_' + fc.name)
+        elif isinstance(fc, VarLenSparseFeat):
+            embedding_layer_dict[fc.name] = Embedding(fc.vocabulary_size + 1, fc.embedding_dim, name='emb_' + fc.name,
+                                                      mask_zero=True)
+
+    return embedding_layer_dict
+
+
+def AutoInt(linear_feature_columns, att_layer_num=3, att_embedding_size=8, att_head_num=2,
+            att_res=True,
+            dnn_hidden_units=(256, 128, 64), dnn_activation='relu', l2_reg_linear=1e-5,
+            l2_reg_embedding=1e-5, l2_reg_dnn=0, dnn_use_bn=False, dnn_dropout=0, seed=1024,
+            task='binary', ):
+    """Instantiates the AutoInt Network architecture.
+    :param linear_feature_columns: An iterable containing all the features used by linear part of the model.
+    :param dnn_feature_columns: An iterable containing all the features used by deep part of the model.
+    :param att_layer_num: int.The InteractingLayer number to be used.
+    :param att_embedding_size: int.The embedding size in multi-head self-attention network.
+    :param att_head_num: int.The head number in multi-head  self-attention network.
+    :param att_res: bool.Whether or not use standard residual connections before output.
+    :param dnn_hidden_units: list,list of positive integer or empty list, the layer number and units in each layer of DNN
+    :param dnn_activation: Activation function to use in DNN
+    :param l2_reg_linear: float. L2 regularizer strength applied to linear part
+    :param l2_reg_embedding: float. L2 regularizer strength applied to embedding vector
+    :param l2_reg_dnn: float. L2 regularizer strength applied to DNN
+    :param dnn_use_bn:  bool. Whether use BatchNormalization before activation or not in DNN
+    :param dnn_dropout: float in [0,1), the probability we will drop out a given DNN coordinate.
+    :param seed: integer ,to use as random seed.
+    :param task: str, ``"binary"`` for  binary logloss or  ``"regression"`` for regression loss
+    :return: A Keras model instance.
+    """
+    if len(dnn_hidden_units) <= 0 and att_layer_num <= 0:
+        raise ValueError("Either hidden_layer or att_layer_num must > 0")
+
+    input_layer_dict = build_input_layers(feature_columns)
+
+    input_layers = list(input_layer_dict.values())
+
+    # 筛选出特征中的sparse特征和dense特征，方便单独处理
+    sparse_feature_columns = list(filter(lambda x: isinstance(x, SparseFeat), feature_columns))
+    dense_feature_columns = list(filter(lambda x: isinstance(x, DenseFeat), feature_columns))
+
+    # 获取dense
+    dnn_dense_input = []
+    for fc in dense_feature_columns:
+        dnn_dense_input.append(input_layer_dict[fc.name])
+    # 将所有的dense特征拼接
+    dnn_dense_input = Concatenate(axis=1)(dnn_dense_input)
+    # 构建embedding字典
+    embedding_layer_dict = build_embedding_layers(feature_columns, input_layer_dict)
+
+    dnn_sparse_embed_input = concat_embedding_list(sparse_feature_columns, input_layer_dict, embedding_layer_dict,
+                                                   flatten=False)
+
+    emb_input = Concatenate(axis=1)(dnn_sparse_embed_input)
+
+    for _ in range(att_layer_num):
+        att_input = InteractingLayer(
+            att_embedding_size, att_head_num, att_res)(emb_input)
+    att_output = tf.keras.layers.Flatten()(att_input)
+
+    dnn_input = Concatenate(axis=1)([att_output, dnn_dense_input])
+
+    if len(dnn_hidden_units) > 0 and att_layer_num > 0:  # Deep & Interacting Layer
+        deep_out = DNN(dnn_hidden_units, dnn_activation, l2_reg_dnn, dnn_dropout, dnn_use_bn, seed=seed)(dnn_input)
+        stack_out = tf.keras.layers.Concatenate()([att_output, deep_out])
+        final_logit = tf.keras.layers.Dense(
+            1, use_bias=False, kernel_initializer=tf.keras.initializers.glorot_normal(seed))(stack_out)
+    elif len(dnn_hidden_units) > 0:  # Only Deep
+        deep_out = DNN(dnn_hidden_units, dnn_activation, l2_reg_dnn, dnn_dropout, dnn_use_bn, seed=seed)(dnn_input, )
+        final_logit = tf.keras.layers.Dense(
+            1, use_bias=False, kernel_initializer=tf.keras.initializers.glorot_normal(seed))(deep_out)
+    elif att_layer_num > 0:  # Only Interacting Layer
+        final_logit = tf.keras.layers.Dense(
+            1, use_bias=False, kernel_initializer=tf.keras.initializers.glorot_normal(seed))(att_output)
+    else:  # Error
+        raise NotImplementedError
+
+    output = tf.math.sigmoid(tf.keras.layers.Dense(1)(dnn_input) + final_logit)
+    model = Model(input_layers, output)
+    return model
+
+
+if __name__ == "__main__":
+    # 读取数据
+
+    samples_data = pd.read_csv("data/movie_sample.txt", sep="\t", header=None)
+    print(samples_data.shape)
+    samples_data.columns = ["user_id", "gender", "age", "hist_movie_id", "hist_len", "movie_id", "movie_type_id",
+                            "label"]
+
+    # samples_data = shuffle(samples_data)
+
+    X = samples_data[["user_id", "gender", "age", "hist_movie_id", "hist_len", "movie_id", "movie_type_id"]]
+    y = samples_data["label"]
+
+    X_train = {"user_id": np.array(X["user_id"]), \
+               "gender": np.array(X["gender"]), \
+               "age": np.array(X["age"]), \
+               "hist_len": np.array(X["hist_len"]), \
+               "movie_id": np.array(X["movie_id"]), \
+               "movie_type_id": np.array(X["movie_type_id"])}
+
+    y_train = np.array(y)
+
+    feature_columns = [SparseFeat('user_id', max(samples_data["user_id"]) + 1, embedding_dim=8),
+                       SparseFeat('gender', max(samples_data["gender"]) + 1, embedding_dim=8),
+                       SparseFeat('age', max(samples_data["age"]) + 1, embedding_dim=8),
+                       SparseFeat('movie_id', max(samples_data["movie_id"]) + 1, embedding_dim=8),
+                       SparseFeat('movie_type_id', max(samples_data["movie_type_id"]) + 1, embedding_dim=8),
+                       DenseFeat('hist_len', 1)]
+
+    print(X_train)
+    n_users = max(samples_data["user_id"]) + 1
+    n_item = max(samples_data["movie_id"]) + 1
+
+    autoint = AutoInt(feature_columns)
+    #
+    autoint.compile('adam',
+                    loss=tf.keras.losses.BinaryCrossentropy(),
+                    metrics=[tf.keras.metrics.BinaryAccuracy(),
+                             tf.keras.metrics.AUC()])
+    autoint.fit(X_train, y_train, batch_size=64, epochs=10, validation_split=0.2, )
+    #
+    print(autoint.summary())
